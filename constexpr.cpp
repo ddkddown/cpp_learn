e5a015b4b2375b4b6d52198ecd8b5057ffa9634f
ddkddown <2628397115@qq.com>
2021-07-02 22:40:57 +0800

    [update] new file

---
+++ constexpr.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+//常量表达式是指值不会改变并且在编译过程中就能得到计算结果的表达式。
+
+int main() {
+    const int max_files = 20; // 是常量表达式, 值不会变，编译时就可确定。
+    const int limit = max_files + 1; //是常量表达式，值不会变，编译时就可确定。
+    int staff_size = 27; //不是常量表达式，值允许在运行时发生改变。
+
+    //contexpr 向编译器表明修饰的变量或函数是常量表达式，可以放心在编译时进行优化，无需等到运行时。
+    //但是编译器不会完全相信程序员，只是对contexpr作为一个参考。
+
+    constexpr int test = 3;
+
+    //这一步会编译出错
+    //test = 4;
+
+    cout<<"test: "<<test<<endl;
+}
